b2bf699792ace6403d8f28d99e2f7877dfb42345
Huybip <155954088+Huybip@users.noreply.github.com>
2025-05-10 08:43:09 +0000

    songs

--- musicPlayer/src/App.java
+++ musicPlayer/src/App.java
@@ -1,9 +1,17 @@
 //import Artist;
+//import Songs;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Music Player Test!");
 
+<<<<<<< HEAD
         
+=======
+        String test = Artist.getArtistName("Artist's ID");
+        System.out.println(test);
+        String test1 = Songs.getSongName("Song's Name");
+        System.out.println(test1);
+>>>>>>> 45295b65280b3a1d9ff4cf6e909f2f7dcb095694
     }
 }
