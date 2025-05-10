a34f679ce745cb7e8a7295bd354f8870e0650a11
quynchi16 <quyntree@gmail.com>
2025-05-10 08:21:42 +0000

    update  main

--- musicPlayer/src/App.java
+++ musicPlayer/src/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Music Player Test!");
 
+        String test = Artist.getArtistName("hello");
+         System.out.println(test);
     }
 }
