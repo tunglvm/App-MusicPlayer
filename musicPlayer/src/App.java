3c58c510752ca8388a92ee1eb36eaeb498869208
quynchi16 <quyntree@gmail.com>
2025-05-10 08:15:21 +0000

    Artist

--- musicPlayer/src/App.java
+++ musicPlayer/src/App.java
@@ -1,5 +1,8 @@
+//import Artist;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Music Player Test!");
+
     }
 }
---
+++ musicPlayer/src/Artist.java
@@ -0,0 +1,17 @@
+class Artist {
+    String Name;
+    String ArtistID;
+    String Information;
+    String Featuring;
+    String Songs;
+    String Album;
+
+    public static String getArtistName(String ArtistID){
+        //lay ten artist
+
+        String test = ArtistID;
+
+        return test;
+    }
+
+}
