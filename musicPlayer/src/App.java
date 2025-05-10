da4749d3514e2e447071413e61509cf93ec9e0d4
tunglvm <dotunglam2k5@gmail.com>
2025-05-10 15:35:29 +0700

    Update class Songs

--- musicPlayer/src/App.java
+++ musicPlayer/src/App.java
@@ -1,10 +1,13 @@
 //import Artist;
+//import Songs;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Music Player Test!");
 
-        String test = Artist.getArtistName("hello");
-         System.out.println(test);
+        String test = Artist.getArtistName("Artist's ID");
+        System.out.println(test);
+        String test1 = Songs.getSongName("Song's Name");
+        System.out.println(test1);
     }
 }
---
+++ musicPlayer/src/Songs.java
@@ -0,0 +1,18 @@
+public class Songs {
+    String Title;
+    String SongID;
+    String Artist;
+    String Type;
+    int Duration;
+
+    public static String getSongName(String SongID){
+        //lay ten artist
+
+        String test1 = SongID;
+
+        return test1;
+    }
+}
+
+
+
