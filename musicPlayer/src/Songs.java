b63bd0d1cd052d05e7c090a6b2b657ef6fe34600
tunglvm <dotunglam2k5@gmail.com>
2025-05-13 10:36:45 +0700

    update constructor

--- musicPlayer/src/Songs.java
+++ musicPlayer/src/Songs.java
@@ -5,6 +5,16 @@ public class Songs {
     String Type;
     int Duration;
 
+    public Songs (String Title, String SongID, String Artist, String Type, int Duration){
+        this.Title = Title;
+        this.SongID = SongID;
+        this.Artist = Artist;
+        this.Type = Type;
+        this.Duration = Duration;
+
+    }
+
+
     public static String getSongName(String SongID){
         //lay Title bai hat
 
