80376de86f609f453d17abc305899647bad31980
quynchi16 <quyntree@gmail.com>
2025-05-17 08:27:35 +0000

    resolved conflict

--- musicPlayer/src/App.java
+++ musicPlayer/src/App.java
@@ -1,24 +1,21 @@
-//import Artist;
-//import Songs;
-//import Album;
-//import Podcast;
-//import User;
-//import Playlist;
 
-import musicPlayer.unittest.testUser;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    testUser.display();
+testUser.display();
 
         
         System.out.println("Music Player Test!");
         
         User user1 = new User("Tùng Lâm", 19, "23010874");
-        System.out.println("User 1: " + user1.getUserID() + "  "+ user1.getName() ); 
+        System.out.println("User 1: " + user1.getUserID() + "  "+ user1.getName() + "\n"); 
 
-        Songs song = new Songs("Lullaby", "IU001s005", "IU", "", "K-Pop", 261);
-        System.out.println("Song: " + song.songTitle + " by " + song.Artist);
+        Group6_OOP_N02_term3_2025.musicPlayer.test.testSongs.getSongName();
+        Songs song1 = new Songs("Lullaby", "IU001s005", "IU", "", "K-Pop", 261);
+        System.out.println("Song: " + song1.songTitle + " by " + song1.Artist);
+        Songs song2 = new Songs("unlucky", "IU001s001", "IU", "", "K-Pop", 231);
+        System.out.println("Song: " + song2.songTitle + " by " + song2.Artist + "\n");
+
 
         Album album = new Album("Love Poem", "IU001", "IU", "Lullaby", "KaKao Entertainment", 1500);
         System.out.println("Album: " + album.albumTitle + " by " + album.Artist + ", copyright by " + album.Copyright);
