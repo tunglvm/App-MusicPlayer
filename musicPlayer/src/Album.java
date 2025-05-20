48062409a0ba0599a0d441359c92e21fe30a9cc7
tunglvm <dotunglam2k5@gmail.com>
2025-05-20 13:48:28 +0700

    lam

--- musicPlayer/src/Album.java
+++ musicPlayer/src/Album.java
@@ -7,6 +7,7 @@ public class Album {
     int albumDuration;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public Album( String AlbumID) {
 
 =======
@@ -15,6 +16,15 @@ public class Album {
 >>>>>>> 314e51f848e92d284d9f0333118fec0888f5e306
         this.AlbumID = AlbumID;
 
+=======
+    public Album(String albumTitle, String AlbumID, Artist Artist, Songs songs, String Copyright, int Duration) {
+        this.albumTitle = albumTitle;
+        this.AlbumID = AlbumID;
+        this.Artist = Artist;
+        this.songs = songs;
+        this.Copyright = Copyright;
+        this.albumDuration = Duration;
+>>>>>>> 314e51f848e92d284d9f0333118fec0888f5e306
     }
 
     public void setAlbumInfo(String Title,Artist Artist, String Songs, String Copyright, int Duration){
