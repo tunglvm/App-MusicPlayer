cfe2e0f6f1b7a30575828b46acdff77ad03a0a3c
Huybip <155954088+Huybip@users.noreply.github.com>
2025-05-20 15:05:29 +0000

    update

--- musicPlayer/test/testSongs.java
+++ musicPlayer/test/testSongs.java
@@ -1,10 +1,24 @@
 package musicPlayer.test;
 
 public class testSongs {
+    public static void main(String[] args) {
+        System.out.println("Song list: ");
+        System.out.println("1. Lullaby");
+        System.out.println("2. unlucky");
+    }
     public static void getSongName(){
         System.out.println("Song list: ");
         System.out.println("1. Lullaby");
         System.out.println("2. unlucky");
     }
+    public static void getSongInfo(){
+        System.out.println("Song Title: Lullaby");
+        System.out.println("Song ID: 1");
+        System.out.println("Artist: Artist A");
+        System.out.println("Album: Album A");
+        System.out.println("Copyright: 2023");
+        System.out.println("Duration: 300 seconds");
+    }
+    
     
 }
