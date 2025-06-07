e5a07e6a4ed2db74362ea9832dccd2209d54adf9
Huybip <155954088+Huybip@users.noreply.github.com>
2025-06-07 10:56:52 +0000

    Transaction

--- musicPlayer/src/musicPlayer/Transaction.java
+++ musicPlayer/src/musicPlayer/Transaction.java
@@ -1,22 +1,23 @@
 package musicPlayer;
 
+/**
+ * Lưu thông tin giao dịch mua/thuê bài hát của người dùng.
+ */
 public class Transaction {
     private String transactionID;
     private String userID;
     private String songID;
-    // "buy" or "rent"
-    private String transactionDate;
     private String artistID;
+    private String transactionType; // "buy" orU "rent"
+    private String transactionDate;
 
-
-
-    public Transaction(String transactionID, String userID, String songID, String transactionDate, String artistID) {
+    public Transaction(String transactionID, String userID, String songID, String artistID, String transactionType,
+            String transactionDate) {
         this.transactionID = transactionID;
         this.userID = userID;
         this.songID = songID;
-       
-       
         this.artistID = artistID;
+        this.transactionType = transactionType;
         this.transactionDate = transactionDate;
     }
 
@@ -44,12 +45,20 @@ public class Transaction {
         this.songID = songID;
     }
 
-    public String getartistID() {
+    public String getArtistID() {
         return artistID;
     }
 
-    public void setartistID(String artistID) {
+    public void setArtistID(String artistID) {
         this.artistID = artistID;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
     }
 
     public String getTransactionDate() {
@@ -59,5 +68,4 @@ public class Transaction {
     public void setTransactionDate(String transactionDate) {
         this.transactionDate = transactionDate;
     }
-    
 }
