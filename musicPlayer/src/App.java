e7d60802261f5247c393db43e1d8733b6dcf9198
quynchi16 <quyntree@gmail.com>
2025-06-26 14:06:53 +0000

    main / app

--- musicPlayer/src/App.java
+++ musicPlayer/src/App.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                new MusicPlayerGUI().setVisible(true);
+
+                // Uncomment the following lines to test the Song class
+                // Ensure the Song class is properly defined in your project
+                // and the file path is correct.  
+                // call Song song constructor
+                // Song song = new Song("mp3MusicPlayer\\test song\\Anh Thanh Nien - HuyR (HQ).mp3"); 
+                // System.out.println(song.getSongTitle());
+                // System.out.println(song.getSongArtist());
+            }
+        });
     }
 }
